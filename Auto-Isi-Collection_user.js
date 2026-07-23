854d037199d517a4be572e4a15ab91f12fefbd6b
zinkmr3-hash <zinkmr3@gmail.com>
2026-07-24 02:34:53 +0700

    Update Auto-Isi-Collection_user.js

--- Auto-Isi-Collection_user.js
+++ Auto-Isi-Collection_user.js
@@ -6,8 +6,8 @@
 // @author       Enhanced
 // @match        https://collection-id.zunamiq.com/*
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/zinkmr3-hash/botsistemdrx/refs/heads/main/auto-adlogs-v4.8.1.user.js
-// @downloadURL  https://raw.githubusercontent.com/zinkmr3-hash/botsistemdrx/refs/heads/main/auto-adlogs-v4.8.1.user.js
+// @updateURL    https://raw.githubusercontent.com/zinkmr3-hash/botsistemdrx/main/Auto-Isi-Collection_user.js
+// @downloadURL  https://raw.githubusercontent.com/zinkmr3-hash/botsistemdrx/main/Auto-Isi-Collection_user.js
 // @run-at       document-idle
 // ==/UserScript==
 function _0x4ef1(_0x3d178e, _0x2fd6e1) {
